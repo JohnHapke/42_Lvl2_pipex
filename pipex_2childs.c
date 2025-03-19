c94b47f41a6f9dc53631a8d84e3b6e258d694c31
John Hapke <mail@john-hapke.de>
2025-03-19 12:39:58 +0100

    update pipex_2childs

--- pipex_2childs.c
+++ pipex_2childs.c
@@ -1,17 +1,52 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   pipex_2childs.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhapke <jhapke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 10:39:41 by jhapke            #+#    #+#             */
-/*   Updated: 2025/03/19 11:33:07 by jhapke           ###   ########.fr       */
+/*   Updated: 2025/03/19 12:39:35 by jhapke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include "libft.h"
+
+void	ft_closefds(int fds[2])
+{
+	close(fds[0]);
+	close(fds[1]);
+}
+
+void	ft_error_handler(int i, char *argv)
+{
+	if (i == 1)
+	{
+		ft_putstr_fd("./pipex: ", 2);
+		ft_putstr_fd(argv, 2);
+		ft_putstr_fd(": ", 2);
+		ft_putstr_fd(strerror(errno), 2);
+		write(2, "\n", 1);
+		exit(1);
+	}
+	else if (i == 2)
+	{
+		perror("pipe");
+		exit(1);
+	}
+	else if (i == 3)
+	{
+		perror("fork");
+		exit(1);
+	}
+	else if (i == 4)
+	{
+		ft_putstr_fd("./pipex: command not found: ", 2);
+		ft_putstr_fd(argv, 2);
+		write(2, "\n", 1);
+	}
+}
 
 void	ft_execution(char *cmd, char **env)
 {
@@ -40,6 +75,63 @@ void	ft_execution(char *cmd, char **env)
 		ft_free(cmd_arg);
 		exit(126);
 	}
+}
+
+void	ft_free(char **path)
+{
+	int	i;
+
+	i = 0;
+	while (path[i])
+		free(path[i++]);
+	free(path);
+}
+
+char	**ft_getenv(char **env)
+{
+	char	**path;
+	int		i;
+
+	path = NULL;
+	i = 0;
+	while (env[i])
+	{
+		if (ft_strncmp(env[i], "PATH=", 5) == 0)
+		{
+			path = ft_split(env[i] + 5, ':');
+			break ;
+		}
+		i++;
+	}
+	return (path);
+}
+
+char	*ft_get_cmd_path(char *cmd, char **env)
+{
+	char	**path;
+	int		i;
+	char	*sub_path;
+	char	*cmd_path;
+
+	path = ft_getenv(env);
+	if (!path)
+		return (NULL);
+	i = 0;
+	while (path[i])
+	{
+		sub_path = ft_strjoin(path[i], "/");
+		cmd_path = ft_strjoin(sub_path, cmd);
+		free(sub_path);
+		if (access(cmd_path, F_OK) == 0)
+		{
+			ft_free(path);
+			return (cmd_path);
+		}
+		free(cmd_path);
+		i++;
+	}
+	ft_free(path);
+	return (NULL);
 }
 
 void	ft_child1(int fds[2], char **argv, char **env)
